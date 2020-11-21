e490b135470d2e60a264a682e1d9551b331e83f8
Herbert Campos <wohoforever@yahoo.com>
2020-11-20 21:36:54 -0800

    Data Structures: Objects, lesson 122

--- Complete_Web_Dev_2021/Javascript/Javascript_webpage/script.js
+++ Complete_Web_Dev_2021/Javascript/Javascript_webpage/script.js
@@ -1,2 +1,21 @@
-var list = ["tiger", "cat", "bear", "bird"];
-console.log(list[0]);
+var user = {
+  name: "John",
+  age: 34,
+  hobby: "Soccer",
+  isMarried: false,
+  spells: ["abrakadabra", "shazam", "boo"],
+  shout: function () {
+    console.log("AAHHHHH");
+  },
+};
+
+var list = [
+  {
+    username: "andy",
+    password: "secret",
+  },
+  {
+    username: "jess",
+    password: "123",
+  },
+];
