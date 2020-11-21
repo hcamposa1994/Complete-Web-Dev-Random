5a19b06658c28f1fe14a4bcc23984953c56a63f5
Herbert Campos <wohoforever@yahoo.com>
2020-11-20 22:34:08 -0800

    Exercise: Build Facebook, lesson 124

--- Complete_Web_Dev_2021/Javascript/Javascript_webpage/script.js
+++ Complete_Web_Dev_2021/Javascript/Javascript_webpage/script.js
@@ -1,21 +1,30 @@
-var user = {
-  name: "John",
-  age: 34,
-  hobby: "Soccer",
-  isMarried: false,
-  spells: ["abrakadabra", "shazam", "boo"],
-  shout: function () {
-    console.log("AAHHHHH");
-  },
-};
-
-var list = [
+var database = [
   {
-    username: "andy",
-    password: "secret",
-  },
-  {
-    username: "jess",
-    password: "123",
+    username: "Alex",
+    password: "supersecret",
   },
 ];
+
+var newsFeed = [
+  {
+    username: "Bobby",
+    timeline: "So tired from all that learning",
+  },
+  {
+    username: "Sally",
+    timeline: "Javascript is soo coool!!",
+  },
+];
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
+
+function signIn(user, pass) {
+  if (user === database[0].username && pass === database[0].password) {
+    console.log(newsFeed);
+  } else {
+    alert("Sorry, wrong username and password");
+  }
+}
+
+signIn(userNamePrompt, passwordPrompt);
