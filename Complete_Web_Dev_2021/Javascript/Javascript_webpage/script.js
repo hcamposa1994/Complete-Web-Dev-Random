cafcd334ac4b5d5fd9768b9685905dcf1e280864
Herbert Campos <wohoforever@yahoo.com>
2020-11-21 00:11:21 -0800

    Exercise: Build Facebook 2, lesson 127

--- Complete_Web_Dev_2021/Javascript/Javascript_webpage/script.js
+++ Complete_Web_Dev_2021/Javascript/Javascript_webpage/script.js
@@ -1,30 +1,50 @@
-// var database = [
-//   {
-//     username: "Alex",
-//     password: "supersecret",
-//   },
-// ];
+var database = [
+  {
+    username: "Alex",
+    password: "supersecret",
+  },
+  {
+    username: "Sally",
+    password: "123",
+  },
+  {
+    username: "Ingrid",
+    password: "777",
+  },
+];
 
-// var newsFeed = [
-//   {
-//     username: "Bobby",
-//     timeline: "So tired from all that learning",
-//   },
-//   {
-//     username: "Sally",
-//     timeline: "Javascript is soo coool!!",
-//   },
-// ];
+var newsFeed = [
+  {
+    username: "Bobby",
+    timeline: "So tired from all that learning",
+  },
+  {
+    username: "Sally",
+    timeline: "Javascript is soo coool!!",
+  },
+];
 
-// var userNamePrompt = prompt("What's your username?");
-// var passwordPrompt = prompt("What's your password?");
+function isUserValid(username, password) {
+  for (var i = 0; i < database.length; i++) {
+    if (
+      database[i].username === username &&
+      database[i].password === password
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
 
-// function signIn(user, pass) {
-//   if (user === database[0].username && pass === database[0].password) {
-//     console.log(newsFeed);
-//   } else {
-//     alert("Sorry, wrong username and password");
-//   }
-// }
+function signIn(username, password) {
+  if (isUserValid(username, password)) {
+    console.log(newsFeed);
+  } else {
+    alert("Sorry, wrong username and password");
+  }
+}
 
-// signIn(userNamePrompt, passwordPrompt);
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
+
+signIn(userNamePrompt, passwordPrompt);
