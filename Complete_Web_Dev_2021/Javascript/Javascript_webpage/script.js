ad2408448e306f2e1fca35d66a0ececd8119bd13
Herbert Campos <wohoforever@yahoo.com>
2020-11-20 23:50:50 -0800

    Loops, lesson 126

--- Complete_Web_Dev_2021/Javascript/Javascript_webpage/script.js
+++ Complete_Web_Dev_2021/Javascript/Javascript_webpage/script.js
@@ -1,30 +1,30 @@
-var database = [
-  {
-    username: "Alex",
-    password: "supersecret",
-  },
-];
+// var database = [
+//   {
+//     username: "Alex",
+//     password: "supersecret",
+//   },
+// ];
 
-var newsFeed = [
-  {
-    username: "Bobby",
-    timeline: "So tired from all that learning",
-  },
-  {
-    username: "Sally",
-    timeline: "Javascript is soo coool!!",
-  },
-];
+// var newsFeed = [
+//   {
+//     username: "Bobby",
+//     timeline: "So tired from all that learning",
+//   },
+//   {
+//     username: "Sally",
+//     timeline: "Javascript is soo coool!!",
+//   },
+// ];
 
-var userNamePrompt = prompt("What's your username?");
-var passwordPrompt = prompt("What's your password?");
+// var userNamePrompt = prompt("What's your username?");
+// var passwordPrompt = prompt("What's your password?");
 
-function signIn(user, pass) {
-  if (user === database[0].username && pass === database[0].password) {
-    console.log(newsFeed);
-  } else {
-    alert("Sorry, wrong username and password");
-  }
-}
+// function signIn(user, pass) {
+//   if (user === database[0].username && pass === database[0].password) {
+//     console.log(newsFeed);
+//   } else {
+//     alert("Sorry, wrong username and password");
+//   }
+// }
 
-signIn(userNamePrompt, passwordPrompt);
+// signIn(userNamePrompt, passwordPrompt);
