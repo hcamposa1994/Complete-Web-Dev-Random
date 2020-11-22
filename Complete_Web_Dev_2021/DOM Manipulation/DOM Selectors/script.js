655aa6d136ba8ef548f3daa52a484ed02dbd76e3
Herbert Campos <wohoforever@yahoo.com>
2020-11-22 00:33:25 -0800

    Exercise: DOM Events, lesson 135

--- Complete_Web_Dev_2021/DOM Manipulation/DOM Selectors/script.js
+++ Complete_Web_Dev_2021/DOM Manipulation/DOM Selectors/script.js
@@ -1,14 +1,30 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var li = document.querySelectorAll("li");
 
 function inputLength() {
   return input.value.length;
 }
 
+function createDeleteButtonElement() {
+  var btn = document.createElement("button");
+  btn.appendChild(document.createTextNode("X"));
+  btn.classList.add("delete");
+  btn.addEventListener("click", function () {
+    btn.parentElement.remove();
+  });
+  return btn;
+}
+
+li.forEach(function (element) {
+  element.append(createDeleteButtonElement());
+});
+
 function createListElement() {
   var li = document.createElement("li");
   li.appendChild(document.createTextNode(input.value));
+  li.appendChild(createDeleteButtonElement());
   ul.appendChild(li);
   input.value = "";
 }
@@ -25,6 +41,15 @@ function addListAfterEnterKeypress(event) {
   }
 }
 
+function toggleDoneClass(event) {
+  var target = event.target;
+  if (!target.classList.contains("delete")) {
+    target.classList.toggle("done");
+  }
+}
+
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterEnterKeypress);
+
+ul.addEventListener("click", toggleDoneClass);
