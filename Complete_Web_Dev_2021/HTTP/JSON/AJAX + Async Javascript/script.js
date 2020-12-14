9b20a63fed1640a0e09908124913860ca593d931
Herbert Campos <wohoforever@yahoo.com>
2020-12-13 20:07:07 -0800

    Promises, lesson 237

---
+++ Complete_Web_Dev_2021/HTTP/JSON/AJAX + Async Javascript/script.js
@@ -0,0 +1,17 @@
+const urls = [
+  "https://jsonplaceholder.typicode.com/users",
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/albums",
+];
+
+Promise.all(
+  urls.map((url) => {
+    return fetch(url).then((respone) => respone.json());
+  })
+)
+  .then((results) => {
+    console.log(results[0]);
+    console.log(results[1]);
+    console.log(results[2]);
+  })
+  .catch(() => console.log("error"));
