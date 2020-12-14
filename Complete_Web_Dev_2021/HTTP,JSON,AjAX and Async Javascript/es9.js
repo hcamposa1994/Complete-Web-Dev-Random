7a3389e60c4946b6f286b963b5558ce835a5a695
Herbert Campos <wohoforever@yahoo.com>
2020-12-14 12:47:35 -0800

    ES9 (ES2018) - Async, lesson 242

--- Complete_Web_Dev_2021/HTTP,JSON,AjAX and Async Javascript/es9.js
+++ Complete_Web_Dev_2021/HTTP,JSON,AjAX and Async Javascript/es9.js
@@ -15,3 +15,17 @@ const objectSpread = (p1, p2, p3) => {
 const { tiger, lion, ...rest } = animals;
 
 objectSpread(tiger, lion, rest);
+
+// For Await Of
+const getData2 = async () => {
+  const arrayOfPromises = urls.map((url) => fetch(url));
+  for await (let request of arrayOfPromises) {
+    const data = await request.json();
+    console.log(data);
+  }
+};
+
+//finally
+// ..then((result) => {
+// }).catch((err) => {
+// }).finally() - always runs regardless of then or catch
