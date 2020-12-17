f8d4e02633deebbca28caeb743f1937b1684c27e
Herbert Campos <wohoforever@yahoo.com>
2020-12-16 21:39:34 -0800

    Postman, lesson 274

--- Complete_Web_Dev_2021/NodeJs-and-ExpressJs/server.js
+++ Complete_Web_Dev_2021/NodeJs-and-ExpressJs/server.js
@@ -2,13 +2,20 @@ import express from "express";
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log(`<h1>Hello</h1>`);
-  next();
-});
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("test");
+  res.send("getting root");
+});
+
+app.get("/profile", (req, res) => {
+  res.send("getting profile");
+});
+
+app.post("/profile", (req, res) => {
+  console.log(req.body);
+  res.send("sucess");
 });
 
 app.listen(3001);
