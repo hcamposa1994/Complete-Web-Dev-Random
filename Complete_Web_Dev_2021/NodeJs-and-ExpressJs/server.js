e03b31710bd51a9350f2ec94537a43c510b9417d
Herbert Campos <wohoforever@yahoo.com>
2020-12-16 20:39:32 -0800

    Express Middleware, lesson 272

--- Complete_Web_Dev_2021/NodeJs-and-ExpressJs/server.js
+++ Complete_Web_Dev_2021/NodeJs-and-ExpressJs/server.js
@@ -2,20 +2,13 @@ import express from "express";
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(`<h1>Hello</h1>`);
+  next();
+});
+
 app.get("/", (req, res) => {
-  res.send("getting root");
-});
-
-app.get("/profile", (req, res) => {
-  res.send("getting profile");
-});
-
-app.post("/profile", (req, res) => {
-  const user = {
-    name: "Sally",
-    hobby: "Soccer",
-  };
-  res.send(user);
+  res.send("test");
 });
 
 app.listen(3001);
